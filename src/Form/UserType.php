11be7cdef52274d652e642411bb4472acbd43170
Dekpo <dekpo@me.com>
2022-10-07 11:53:34 +0200

    Added PasswordType to the password field in UserType Form

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -7,14 +7,17 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+
+
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('email')
-            ->add('roles')
-            ->add('password')
+            // ->add('roles')
+            ->add('password',PasswordType::class)
         ;
     }
 
